4c7effe34ade3d783bc5187f1b6d5bf7f41da458
sumitha <sumithas2@yahoo.com>
2017-03-17 12:35:08 -0400

    Q4 product views with includes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,3 @@
 class ProductsController < ApplicationController
 
-	def index
-    @products = Product.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @users }
-    end
-  end
 end
